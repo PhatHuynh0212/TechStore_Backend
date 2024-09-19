c4dc0929b34c72313bb02b3d6ab9d53ddd325618
huynhthanhphat <thanhphat02122003@gmail.com>
2024-09-20 04:30:50 +0700

    add cors and check conditions of user

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -8,7 +8,7 @@ const createUser = async (req, res) => {
         const reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
         const isCheckEmail = reg.test(email);
         // Check information
-        if (!name || !email || !password || !confirmPassword || !phone) {
+        if (!email || !password || !confirmPassword) {
             return res.status(200).json({
                 status: "ERR",
                 message: "The input is required",
@@ -37,11 +37,11 @@ const createUser = async (req, res) => {
 const loginUser = async (req, res) => {
     try {
         // console.log(req.body);
-        const { name, email, password, confirmPassword, phone } = req.body;
+        const { email, password } = req.body;
         const reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
         const isCheckEmail = reg.test(email);
         // Check information
-        if (!name || !email || !password || !confirmPassword || !phone) {
+        if (!email || !password) {
             return res.status(200).json({
                 status: "ERR",
                 message: "The input is required",
@@ -50,11 +50,6 @@ const loginUser = async (req, res) => {
             return res.status(200).json({
                 status: "ERR",
                 message: "The input is email",
-            });
-        } else if (password !== confirmPassword) {
-            return res.status(200).json({
-                status: "ERR",
-                message: "The password is equal confirmPassword",
             });
         }
         // Get request body into service
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -11,7 +11,7 @@ const createUser = (newUser) => {
             });
             if (checkUser !== null) {
                 resolve({
-                    status: "OK",
+                    status: "ERR",
                     message: "The email is already exist",
                 });
             }
@@ -40,7 +40,7 @@ const createUser = (newUser) => {
 
 const loginUser = (userLogin) => {
     return new Promise(async (resolve, reject) => {
-        const { name, email, password, confirmPassword, phone } = userLogin;
+        const { email, password } = userLogin;
         try {
             const checkUser = await User.findOne({
                 email: email,
@@ -48,7 +48,7 @@ const loginUser = (userLogin) => {
 
             if (checkUser === null) {
                 resolve({
-                    status: "OK",
+                    status: "ERR",
                     message: "The user is not defined",
                 });
             }
@@ -60,7 +60,7 @@ const loginUser = (userLogin) => {
 
             if (!comparePassword) {
                 resolve({
-                    status: "OK",
+                    status: "ERR",
                     message: "The password or username is incorrect",
                 });
             }

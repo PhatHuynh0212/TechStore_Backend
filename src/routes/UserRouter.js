5a896661657b78e17604c7296a468207c9ecdde2
huynhthanhphat <thanhphat02122003@gmail.com>
2024-10-02 17:08:27 +0700

    fix

--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -11,7 +11,7 @@ router.post("/sign-in", UserController.loginUser);
 router.post("/log-out", UserController.logoutUser);
 router.put("/update-user/:id", authUserMiddleWare, UserController.updateUser);
 router.delete("/delete-user/:id", authMiddleWare, UserController.deleteUser);
-router.get("/getAll", authMiddleWare, UserController.getAllUser);
+router.get("/get-all", authMiddleWare, UserController.getAllUser);
 router.get(
     "/get-details/:id",
     authUserMiddleWare,
